e66f453af8e8366b50987a478a071940582c5db2
Halí V. <halivelazquez@outlook.com>
2023-11-27 16:41:54 -0600

    feat: end 240

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -7,6 +7,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { InputComponent } from '../input/input.component';
+import { DateFormControl } from '../date-form-control';
 
 @Component({
   selector: 'app-card-form',
@@ -23,7 +24,7 @@ export class CardFormComponent {
       Validators.minLength(16),
       Validators.maxLength(16),
     ]),
-    expiration: new FormControl('', [
+    expiration: new DateFormControl('', [
       Validators.required,
       Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/),
     ]),
