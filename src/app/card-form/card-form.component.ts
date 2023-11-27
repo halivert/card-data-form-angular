fffef30f1ad491100cc5478155a4e56361e89aeb
Halí V. <halivelazquez@outlook.com>
2023-11-27 16:05:19 -0600

    feat: end 236

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,16 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { InputComponent } from '../input/input.component';
 
 @Component({
   selector: 'app-card-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, InputComponent],
   templateUrl: './card-form.component.html',
   styleUrl: './card-form.component.css',
 })
 export class CardFormComponent {
   cardForm = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    expiration: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/),
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+    ]),
   });
+
+  onSubmit() {
+    console.log('Submit');
+  }
 }
---
+++ src/app/input/input.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css',
+})
+export class InputComponent {
+  // @ts-expect-error
+  @Input() control: FormControl;
+  @Input() label = '';
+
+  constructor() {}
+
+  showErrors(): boolean {
+    return !!(
+      this.control.touched &&
+      this.control.dirty &&
+      this.control.errors
+    );
+  }
+}
